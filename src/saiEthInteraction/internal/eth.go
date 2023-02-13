e78bd2723c8547fe037f4d9bb8c4be95c550b56d
Valeriy Stoyanov <valeriy.stoyanov@smile.fr>
2023-02-13 20:58:45 +0200

    Nonce fix

--- src/saiEthInteraction/internal/eth.go
+++ src/saiEthInteraction/internal/eth.go
@@ -3,8 +3,8 @@ package internal
 import (
 	"context"
 	"crypto/ecdsa"
-	"log"
 	"math/big"
+	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -15,40 +15,62 @@ import (
 	"go.uber.org/zap"
 )
 
-func (s *InternalService) RawTransaction(client *ethclient.Client, value *big.Int, data []byte, contract *models.Contract) (string, error) {
+var mux sync.Mutex
+var nonceList = map[string]uint64{}
+
+func (is *InternalService) getNonce(client *ethclient.Client, contract *models.Contract, fromAddress common.Address) (uint64, error) {
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return 0, err
+	}
+
+	mux.Lock()
+	if prevNonce, ok := nonceList[contract.Address]; ok {
+		if prevNonce == nonce {
+			nonce += 10
+		}
+	}
+
+	nonceList[contract.Address] = nonce
+	mux.Unlock()
+
+	return nonce, nil
+}
+
+func (is *InternalService) RawTransaction(client *ethclient.Client, value *big.Int, data []byte, contract *models.Contract) (string, error) {
 	d := time.Now().Add(5000 * time.Millisecond)
 	ctx, cancel := context.WithDeadline(context.Background(), d)
 	defer cancel()
 
 	privateKey, err := crypto.HexToECDSA(contract.Private)
 	if err != nil {
-		s.Logger.Error("handlers - api - RawTransaction - HexToECDSA", zap.Error(err))
+		is.Logger.Error("handlers - api - RawTransaction - HexToECDSA", zap.Error(err))
 		return "", err
 	}
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
-		s.Logger.Error("handlers - api - RawTransaction - cast publicKey to ecdsa", zap.Error(err))
+		is.Logger.Error("handlers - api - RawTransaction - cast publicKey to ecdsa", zap.Error(err))
 		return "", err
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	nonce, err := is.getNonce(client, contract, fromAddress)
 	if err != nil {
-		log.Println(err)
+		is.Logger.Error("handlers - api - RawTransaction - get nonce", zap.Error(err))
+		return "", err
 	}
 
 	toAddress := common.HexToAddress(contract.Address)
 
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
-		s.Logger.Error("handlers - api - RawTransaction - get suggested gas price", zap.Error(err))
+		is.Logger.Error("handlers - api - RawTransaction - get suggested gas price", zap.Error(err))
 		return "", err
 	}
 
-	s.Logger.Sugar().Debugf("GAS PRICE : %v", gasPrice)
+	is.Logger.Sugar().Debugf("GAS PRICE : %v", gasPrice)
 
 	tx := types.NewTx(&types.LegacyTx{
 		Nonce:    nonce,
@@ -61,19 +83,19 @@ func (s *InternalService) RawTransaction(client *ethclient.Client, value *big.In
 
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
-		s.Logger.Error("handlers - api - RawTransaction - get networkID", zap.Error(err))
+		is.Logger.Error("handlers - api - RawTransaction - get networkID", zap.Error(err))
 		return "", err
 	}
 
 	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
 	if err != nil {
-		s.Logger.Error("handlers - api - RawTransaction - signTx", zap.Error(err))
+		is.Logger.Error("handlers - api - RawTransaction - signTx", zap.Error(err))
 		return "", err
 	}
 
 	err = client.SendTransaction(ctx, signedTx)
 	if err != nil {
-		s.Logger.Error("handlers - api - RawTransaction - sendTx", zap.Error(err))
+		is.Logger.Error("handlers - api - RawTransaction - sendTx", zap.Error(err))
 		return "", err
 	}
 
